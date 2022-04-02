fe79c5130d8da51cdb40407e8d5a1e56ddf0687c
ErickFPCruz <erick.cruz97@hotmail.com>
2022-04-01 22:24:38 -0300

    Porta de Entrada com Senha

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Por favor, escreva tua senha aqui: ");
+
+bool senhaDoUsuario = Console.ReadLine() == "umaSenhaIncrivelmenteSegura";
+
+if (senhaDoUsuario)
+{
+    Console.WriteLine("Acesso permitido!");
+}
+
+else
+{
+    Console.WriteLine("Acesso negado!");
+
+}
